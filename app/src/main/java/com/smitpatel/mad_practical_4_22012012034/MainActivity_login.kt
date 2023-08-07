b40aaac7f64d89bb05e9394dd27bf1db6be6dece
smit1 <Smit02042004@gmail.com>
2023-08-07 10:06:31 +0530

    07-08-2023

---
+++ app/src/main/java/com/smitpatel/mad_practical_4_22012012034/MainActivity_login.kt
@@ -0,0 +1,11 @@
+package com.smitpatel.mad_practical_4_22012012034
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_login)
+    }
+}
