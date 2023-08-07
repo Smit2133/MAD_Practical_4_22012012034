bc4c641462776552bc8064a2d7221cd66f50a760
smit1 <Smit02042004@gmail.com>
2023-08-07 10:46:29 +0530

    07-08-2023

--- app/src/main/java/com/smitpatel/mad_practical_4_22012012034/MainActivity.kt
+++ app/src/main/java/com/smitpatel/mad_practical_4_22012012034/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.smitpatel.mad_practical_4_22012012034
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var Login_button=findViewById<Button>(R.id.LoginButton)
+        var SignUp_button=findViewById<Button>(R.id.Sign_up)
+        Login_button.setOnClickListener(){
+            Intent(this@MainActivity,MainActivity_login::class.java).also { startActivity(it)}
+        }
+        SignUp_button.setOnClickListener(){
+            Intent(this@MainActivity,MainActivity_Register::class.java).also { startActivity(it)}
+        }
     }
 }
